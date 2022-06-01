9f3755c8feca7f9f48a2b0e6ecac6918c09747cb
Yan Luz <87395020+Yagami5735@users.noreply.github.com>
2022-06-01 19:07:36 -0300

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks import ev3brick 
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+
+ev3 = EV3Brick()
+
+motorR = Motor(Port.A)
+motorL = Motor(Port.D)
+
+eyes = UltrasonicSensor(Port.S1)
+
+
+
+def motorStop():
+    motorR.run(0)
+    motorL.run(0)
+    
+
+def motorFoward():
+    motorR.run(1000)
+    motorL.run(1000)
+    
+
+def motorBack():
+    motorR.run(-1000)
+    motorL.run(-1000)
+    
+
+def motorTurnRight():
+    motorR.run(700)
+    wait(500)
+    motorL.run(1000)
+
+
+def motorTurnLeft():
+    motorL.run(700)
+    wait(500)
+    motorR.run(1000)
+
+
+def motorSpin():
+    motorR.run(400)
+    motorL.run(-400)
+
+
+search = False
+
+while True:
+    a = eyes.distance()
+    while search == False:
+        
+        b = ev3brick.buttons()
+        if Button.UP in b:
+            motorFoward()
+            wait(3000)
+            motorStop()
+
+        elif Button.DOWN in b:
+            motorBack()
+            wait(3000)
+            motorStop()
+
+        elif Button.RIGHT in b:
+            motorTurnRight()
+            wait(1000)
+            motorFoward()
+            wait(3000)
+            motorStop()
+
+        elif Button.LEFT in b:
+            motorTurnLeft()
+            wait(1000)
+            motorFoward()
+            wait(3000)
+            motorStop()
+    
+        elif Button.CENTER in b:
+            search = True
+
+    while search == True:
+        a = eyes.distance()
+        print(a)
+        if a <= 400:
+            motorSpin()
+    
+        else:
+            motorFoward()
+        
+        if Button.UP in b:
+            motorStop()
+            search = False
+            
+
+    
+    
